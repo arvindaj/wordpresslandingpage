736d825ea647475a8422f4e26e3f2c88961e7bc0
arvindj <jeevaarvind489@gmail.com>
2025-08-06 21:51:18 +0530

    commit

--- src/component/portfoliosection.jsx
+++ src/component/portfoliosection.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import user4 from "../assets/img/portfolio4.png";
-import user3 from "../assets/img/portfolio3.png";
 import user2 from "../assets/img/portfolio2.1.png";
 import user1 from "../assets/img/portfolio1.png";
 import { FaCheckCircle } from "react-icons/fa";
 import { BsArrowUpCircle } from "react-icons/bs";
-import '../assets/css/portfoliosection.css'
+
 
 
 
@@ -28,7 +27,7 @@ const PortfolioSection = () => {
         {/* Grid */}
         <div className="row g-4">
           {/* First Project Card */}
-          <div className="col-12 col-sm-6 col-md-3">
+          <div className="col-12 col-md-6 col-lg-3">
             <div className="card border-0 h-100">
               <img src={user1} className="card-img-top img-fluid" alt="MLON Project" />
               <div className="card-body d-flex flex-column justify-content-between">
@@ -45,18 +44,13 @@ const PortfolioSection = () => {
           </div>
 
           {/* Custom Project */}
-          <div className="col-12 col-sm-6 col-md-4">
+          <div className="col-12 col-md-6 col-lg-4">
             <div className="card bg-dark text-white border-0 h-100">
               <img src={user2} className="card-img-top img-fluid" alt="Custom Project" />
               <div className="card-body position-relative" style={{ top: "10px" }}>
-                 <span className="btn mb-lg-3 mt-lg-2" style={{
-                          backgroundColor:'#8EC440'
-                        }}>Custom Project</span>
+                <span className="btn mb-lg-3 mt-lg-2" style={{ backgroundColor: '#8EC440' }}>Custom Project</span>
                 <h1 className="card-title mx-1 fs-1 mt-2">Create Your Custom Project</h1>
-                <a
-                  href="#"
-                  className="text-white fw-semibold mt-lg-4 d-flex align-items-center fs-4 mx-2"
-                >
+                <a href="#" className="text-white fw-semibold mt-lg-4 d-flex align-items-center fs-4 mx-2">
                   Learn more
                   <FaCheckCircle className="ms-2" style={{ color: '#8EC440' }} />
                 </a>
@@ -65,53 +59,49 @@ const PortfolioSection = () => {
           </div>
 
           {/* Blog Series Card */}
-          <div className="col-12 col-md-5">
-            <div className="card border-0 h-100 ">
-              <div className="card-body bg-light mb-4 ">
+          <div className="col-12 col-md-12 col-lg-5">
+            <div className="card border-0 h-100">
+              <div className="card-body bg-light mb-4">
                 <h1 className="card-title fw-bold fs-1">
                   Building Authority Through Storytelling
                 </h1>
                 <p className="card-text fs-4 mt-5">
                   Create a blog series and video content to position the brand as an industry leader.
                 </p>
-                <a href="#" className=" text-black fs-5">Learn more <BsArrowUpCircle className="ms-2" /></a>
+                <a href="#" className=" text-black fs-5">
+                  Learn more <BsArrowUpCircle className="ms-2" />
+                </a>
               </div>
 
               <div className='row g-3'>
-                <div className='col-12 col-sm-6'>
+                {/* WordPress Web Service */}
+                <div className='col-12 col-md-6'>
                   <div className="card bg-dark text-white shadow-sm border-0 h-100">
                     <div className="card-body d-flex flex-column justify-content-between">
                       <div>
-                        <span className="btn mb-2 mt-2 fs-6" style={{
-                          backgroundColor:'#8EC440'
-                        }}>WordPress Web Service</span>
+                        <span className="btn mb-2 mt-2 fs-6" style={{ backgroundColor: '#8EC440' }}>WordPress Web Service</span>
                         <h5 className=" fs-3 mt-4">
                           Engaging Your Audience, One Post at a Time
                         </h5>
                       </div>
-                     <a
-                  href="#"
-                  className="text-white fw-semibold mb-4 d-flex align-items-center fs-4 mx-2"
-                >
-                  Learn more
-                  <FaCheckCircle className="ms-2" style={{ color: '#8EC440' }} />
-                </a>
+                      <a href="#" className="text-white fw-semibold mb-4 d-flex align-items-center fs-4 mx-2">
+                        Learn more
+                        <FaCheckCircle className="ms-2" style={{ color: '#8EC440' }} />
+                      </a>
                     </div>
                   </div>
                 </div>
 
-                {/* WordPress Web Service */}
-                <div className='col-12 col-sm-6'>
+                <div className='col-12 col-md-6'>
                   <div className="text-center">
                     <img src={user4} alt="Start Website" className="img-fluid mb-2" />
-                    {/* <a href="#" className="btn btn-dark w-100">Start Your Website Today</a> */}
                   </div>
                 </div>
               </div>
             </div>
           </div>
-
         </div>
+
       </div>
     </section>
   );
